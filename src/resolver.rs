73ead268f863863f974fd4bf5d91281957792e00
pacifistes <bbrunell@student.42.fr>
2019-04-24 21:37:10 +0200

    ajout des asserts dans les functions de resolution

--- src/resolver.rs
+++ src/resolver.rs
@@ -4,16 +4,14 @@ pub fn negative_discriminant(a: f64, b: f64, c: f64, d: f64) {
     println!("({} - i * sqrt({})) / {})", -b, -d, a * 2.0);
 }
 
-pub fn null_discriminant(a: f64, b: f64, c: f64, d: f64) {
+pub fn null_discriminant(a: f64, b: f64) {
+    assert_ne!(a, 0.00);
     println!("Discriminant is null, the solutions is :");
     println!("{}", -b / (2.0 * a));
 }
 
 pub fn positive_discriminant(a: f64, b: f64, c: f64, d: f64) {
-    if a == 0.0 {
-        println!("je sais pas encore");
-        return;
-    }
+    assert_ne!(a, 0.00);
     println!("Discriminanat is strictly positive, the two solutions are: ");
     println!("{}", (-b - d.sqrt()) / (2.0 * a));
     println!("{}", (-b + d.sqrt()) / (2.0 * a));
@@ -25,15 +23,12 @@ pub fn second_degree(a: f64, b: f64, c: f64) {
     match d {
         negative if negative < 0.0 => negative_discriminant(a, b, c, d),
         positive if positive > 0.0 => positive_discriminant(a, b, c, d),
-        _ => null_discriminant(a, b, c, d),
+        _ => null_discriminant(a, b),
     };
 }
 
 pub fn first_degree(b: f64, c: f64) {
-    if b == 0.0 {
-        println!("je sais pas encore");
-        return;
-    }
+    assert_ne!(b, 0.00);
     println!("The solution is:");
     println!("{}", c / b);
 }
